ed84553edb7fc0ad5032d78206fa869b565f808b
Esteban-L-tech <landaisesteba@gmail.com>
2025-03-06 13:53:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
-function App() {
+function App(){
+  const [fromCurrency, setFromCurrency] = useState('USD');
+  const [currencies, setCurrencies] = useState([]);
+  const [toCurrency, setToCurrency] = useState('EUR');
+  const [exchangeRate, setExchangeRate] = useState();
+  const [amount, setAmount] = useState(100);
+  const [convertedAmount, setConvertedAmount] = useState(null);
+
+useEffect(() => {
+  const fetchExchangeRate = async () => {
+    const reponse = await axios.get('https://api.exchangerate-api.com/v4/latest/USD');
+    setExchangeRate(reponse.data.rates);
+    setCurrencies(Object.keys(reponse.data.rates));
+  };
+  fetchExchangeRate();
+},[]);
+
+useEffect(() => {
+  if(exchangeRate){
+    const rateForm = exchangeRate[fromCurrency];
+    const rateTo = exchangeRate[toCurrency];
+
+    setConvertedAmount((amount*rateTo)/rateForm);
+  }
+},[amount, fromCurrency, toCurrency, exchangeRate]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <h1>Convertisseur de devise</h1>
+
+      <select value={fromCurrency} onChange={(e) => setFromCurrency(e.target.value)}>
+        {currencies.map((currency) => (
+          <option key={currency} value={currency}>
+            {currency}
+          </option>
+        ))}
+      </select>
+
+      <select value={toCurrency} onChange={(e) => setToCurrency(e.target.value)}>
+        {currencies.map((currency) => (
+          <option key={currency} value={currency}>
+            {currency}
+          </option>
+        ))}
+      </select>
+
+      <input
+       type="number"
+       value={amount}
+       onChange = {(e) => setAmount(e.target.value)}
+       placeholder="Veuillez écrire le montant à convertir"
+      />
+
+      {convertedAmount !== null ? (
+        <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {amount} {fromCurrency} = {convertedAmount.toFixed(2)} {toCurrency}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      ):(
+        <p>
+          Données en cours de chargement...
+        </p>
+      )}
+
+
     </div>
   );
 }
