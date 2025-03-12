8b0ae90e092046c0040786267076c05ca5b981bf
Esteban-L-tech <landaisesteba@gmail.com>
2025-03-12 14:50:30 +0100

    pp

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,6 +1,6 @@
 import InputGroup from 'react-bootstrap/InputGroup';
 import NewCarousel from '../compos/carouselNews';
-import { Row } from 'react-bootstrap';
+import { Row, Container } from 'react-bootstrap';
 import React from "react";
 import NewCarouselNews from '../compos/carouselNews';
 let VerticalBanner = './asset/image/bande_img.jpg'
@@ -22,7 +22,7 @@ function News() {
     { title: 'Titre de l’article', image: '/assets/images/img1.jpg', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris bibendum orci quis diam malesuada, quis pharetra risus elementum. Ut mollis  posuere fermentum. Sed ut sem eu orci blandit rutrum nec vitae lectus.  Suspendisse suscipit et neque in porttitor. ' },
   ];
 
-  const carouselItems3 = [
+  const carouselNews3 = [
     { title: 'Titre de l’article', image: '/assets/images/img1.jpg', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris bibendum orci quis diam malesuada, quis pharetra risus elementum. Ut mollis  posuere fermentum. Sed ut sem eu orci blandit rutrum nec vitae lectus.  Suspendisse suscipit et neque in porttitor. ' },
     { title: 'Titre de l’article', image: '/assets/images/img1.jpg', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris bibendum orci quis diam malesuada, quis pharetra risus elementum. Ut mollis  posuere fermentum. Sed ut sem eu orci blandit rutrum nec vitae lectus.  Suspendisse suscipit et neque in porttitor. ' },
     { title: 'Titre de l’article', image: '/assets/images/img1.jpg', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris bibendum orci quis diam malesuada, quis pharetra risus elementum. Ut mollis  posuere fermentum. Sed ut sem eu orci blandit rutrum nec vitae lectus.  Suspendisse suscipit et neque in porttitor. ' },
@@ -36,38 +36,18 @@ function News() {
         <p>Actualités</p>
         <p>Dernière news</p>
       </div>
-
+      <Container>
       <NewCarouselNews items={carouselNews1} />
-      <div className="container mx-auto text-center">
-        <div className="row">
-          <section id="aboutUs">
-          </section>
-        </div>
-      </div>
+      </Container>
+      
 
-      <div className='test'>
-        <p>Développement informatique</p>
-      </div>
-
+      <Container>
       <NewCarouselNews items={carouselNews2} />
-      <div className="container mx-auto text-center">
-        <div className="row">
-          <section id="aboutUs">
-          </section>
-        </div>
-      </div>
+      </Container>
 
-      <div className='test'>
-        <p>Technologies</p>
-      </div>
-
-      <NewCarouselNews items={carouselItems3} />
-      <div className="container mx-auto text-center">
-        <div className="row">
-          <section id="aboutUs">
-          </section>
-        </div>
-      </div>
+      <Container>
+      <NewCarouselNews items={carouselNews3} />
+      </Container>
     </>
   )
 }
